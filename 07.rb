7a32f965544ceee77c4fe089a5797adfac1c57a7
Ikhsan Assaat <ixnixnixn@yahoo.com>
2020-12-09 05:15:45 +0000

    Refactor day 7

--- 07.rb
+++ 07.rb
@@ -1,56 +1,53 @@
-require 'set'
-
-input = File.readlines('07.txt', chomp: true)
-
-Bag = Struct.new(:q, :name)
+input = File.readlines('sample.txt', chomp: true)
+# input = File.readlines('07.txt', chomp: true)
 
 graph = {}
 i_graph = {}
+
+Bag = Struct.new(:q, :color)
+
 input.each { |l|
-  bag, con = l.split(' bags contain ')
-  con_bags = con.split(/ bags?[.,]/).map { |con_bag| 
-    con_bag = con_bag.strip
-    next nil if con_bag == 'no other'
-    Bag.new(con_bag[0..1].to_i, con_bag[2..])
+  bag, content_str = l.split(' bags contain ')
+  contents = content_str.split(/ bags?[.,]/).map { |c|
+    c = c.strip
+    q = c[0..1].to_i
+    next nil if q == 0
+    Bag.new(q, c[2..])
   }.compact
   
-  # containing
-  graph[bag] = con_bags
-  
   # contained
-  con_bags.each { |con_bag|
-    set = i_graph[con_bag.name]
-    set = Set.new([]) if set.nil?
-    set << bag
-    i_graph[con_bag.name] = set
+  graph[bag] = contents
+
+  # contained_by
+  contents.each {|b| 
+    i_graph[b.color] = [] if i_graph[b.color].nil?
+    i_graph[b.color] << bag
   }
 }
 
-def count(graph, expected)
-  return Set.new([]) if graph[expected].nil?
+def count(graph, bag_color)
+  return [] if graph[bag_color].nil?
 
-  res = Set.new(graph[expected])
-  graph[expected].each { |bag| 
-    res += count(graph, bag)
+  res = graph[bag_color]
+  graph[bag_color].each { |color|
+    res += count(graph, color)
+  }
+  res.uniq
+end
+
+def count2(graph, bag_color)
+  return 0 if graph[bag_color].empty?
+
+  res = 0
+  graph[bag_color].each { |bag| 
+    res += bag.q + bag.q * count2(graph, bag.color)
   }
   res
 end
 
-def count2(graph, bag)
-  return 0 if graph[bag].empty?
-
-  graph[bag].reduce(0) { |acc, bag|
-    acc + bag.q + bag.q * count2(graph, bag.name)
-  }
-end
-
-# puts graph
-# puts i_graph
-
 res1 = count(i_graph, 'shiny gold').count
 res2 = count2(graph, 'shiny gold')
-
 puts '*' * 20
-puts res1
-puts res2
+puts (res1 == 121) ? "🎉" : "💔 (#{res1})"
+puts (res2 == 3805) ? "🎉" : "💔 (#{res2})"
 puts '*' * 20
